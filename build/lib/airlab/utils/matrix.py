bbf671e6b13ad0628643610524b2428acfe3350f
Luis Torres <ltorres6@wisc.edu>
2021-11-23 23:04:50 -0600

    Fix mask negation. Fix create_graph_3d

--- build/lib/airlab/utils/matrix.py
+++ build/lib/airlab/utils/matrix.py
@@ -14,19 +14,18 @@
 
 import torch as th
 
-
-class MatrixDiagonalElement:
-    def __init__(self, edge_index, edge_values, offset, dtype=th.float32, device="cpu"):
+class MatrixDiagonalElement():
+    def __init__(self, edge_index, edge_values, offset, dtype=th.float32, device='cpu'):
         self.edge_index = th.from_numpy(edge_index).to(dtype=th.int64, device=device)
         self.edge_values = th.from_numpy(edge_values).to(dtype=dtype, device=device)
         self.offset = offset.to(dtype=th.int64, device=device)
 
-
-class LaplaceMatrix:
-    def __init__(self, number_of_nodes, diag_elements, dtype=th.float32, device="cpu"):
+class LaplaceMatrix():
+    def __init__(self, number_of_nodes, diag_elements, dtype=th.float32, device='cpu'):
         self.main_diag = th.zeros(int(number_of_nodes), dtype=dtype, device=device)
         self.diag_elements = diag_elements
         self.size = int(number_of_nodes)
+
 
         self.update()
 
@@ -46,6 +45,7 @@ class LaplaceMatrix:
             mat[diag.edge_index[-1, :] + diag.offset, diag.edge_index[-1, :]] = diag.edge_values
 
         return mat
+
 
 
 def band_mv(A, x):
@@ -78,17 +78,18 @@ def expm_krylov(A, x, phi=1, krylov_dim=30, inplace=True):
     Q = th.zeros(x.size()[0], krylov_dim, dtype=x.dtype, device=x.device)
     T = th.zeros(krylov_dim + 1, krylov_dim + 1, dtype=x.dtype, device=x.device)
 
-    # compute the norm of the vector
+    #compute the norm of the vector
     norm_x = th.norm(x, p=2)
 
     # normalize vector
-    q = x / norm_x
+    q = x/norm_x
     Q[:, 0] = q.clone()
 
     r = band_mv(A, q)
 
     T[0, 0] = th.dot(q, r)
     r = r - q.mul(T[0, 0])
+
 
     T[0, 1] = th.norm(r, p=2) + 1e-10
     T[1, 0] = T[0, 1]
@@ -97,15 +98,15 @@ def expm_krylov(A, x, phi=1, krylov_dim=30, inplace=True):
         b = q
         q = r
 
-        q.div_(T[k - 1, k])
+        q.div_(T[k -1, k])
 
-        Q[:, k] = q.clone()
+        Q[:,k] = q.clone()
 
-        r = band_mv(A, q) - b.mul_(T[k - 1, k])
+        r = band_mv(A, q) - b.mul_(T[k -1, k])
 
         T[k, k] = th.dot(q, r)
 
-        r = r - q.mul(T[k, k])
+        r =  r - q.mul(T[k, k])
 
         T[k + 1, k] = th.norm(r, p=2) + 1e-10
         T[k, k + 1] = T[k + 1, k]
@@ -114,8 +115,14 @@ def expm_krylov(A, x, phi=1, krylov_dim=30, inplace=True):
     exp_mat = expm_eig(T[:-1, :-1])
 
     if inplace:
-        th.mv(Q, exp_mat[:, 0], out=x)
+        th.mv(Q, exp_mat[:,0], out=x)
         x.mul_(norm_x)
     else:
-        return th.mv(Q, exp_mat[:, 0]).mul_(x)
+        return  th.mv(Q, exp_mat[:,0]).mul_(x)
+
+
+
+
+
+
 
